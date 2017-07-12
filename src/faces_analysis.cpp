9b61d6c0d0afa1be88f951ee9831bd9ed1a90873
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2017-07-12 15:55:39 +0100

    Completed integration of CMU openpose
    Obviously requires CMU openpose to be installed.
    
    faces_analysis now creates a poses.yaml file containing, for every
    frame, 18 points for the skeleton, 70 for the face (including pupils),
    21 for each of the hands.
    
    Run faces_analysis --help for options

--- src/faces_analysis.cpp
+++ src/faces_analysis.cpp
@@ -37,6 +37,63 @@ using namespace cv;
 namespace enc = sensor_msgs::image_encodings;
 namespace po = boost::program_options;
 
+
+const string POSES_FILE ("poses.yaml");
+
+/**
+ * Format of poses.yaml:
+ * details of parts idx is here: https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/doc/output.md
+
+<topic_name>:
+    - poses:
+            1: # pose index
+            # x,y in image coordinate, c is confidence in [0.0,1.0]
+            - [x, y, c] # Nose
+            - [x, y, c] # Neck
+            - [x, y, c] # RShoulder
+            - [x, y, c] # RElbow
+            - [x, y, c] # RWrist
+            - [x, y, c] # LShoulder
+            - [x, y, c] # LElbow
+            - [x, y, c] # LWrist
+            - [x, y, c] # RHip
+            - [x, y, c] # RKnee
+            - [x, y, c] # RAnkle
+            - [x, y, c] # LHip
+            - [x, y, c] # LKnee
+            - [x, y, c] # LAnkle
+            - [x, y, c] # REye
+            - [x, y, c] # LEye
+            - [x, y, c] # REar
+            - [x, y, c] # LEar
+            2:
+            - ...
+      faces:
+            1: # face index
+            # x,y in image coordinate, c is confidence in [0.0,1.0]
+            - [x, y, c]
+            - ... # 70 points in total
+            2:
+            - ...
+      hands:
+            1: # hand index
+                left:
+                # x,y in image coordinate, c is confidence in [0.0,1.0]
+                - [x, y, c]
+                - ... # 20 points in total
+                right:
+                # x,y in image coordinate, c is confidence in [0.0,1.0]
+                - [x, y, c]
+                - ... # 20 points in total
+            2:
+            ...
+    - poses: # 2nd frame
+        ...
+
+
+*/
+
+
 bool interrupted = false;
 
 void my_handler(int s){
@@ -44,6 +101,68 @@ void my_handler(int s){
     interrupted = true; 
 }
 
+typedef op::Array<float> opPosesKeypoints;
+typedef op::Array<float> opFacesKeypoints;
+typedef std::array<op::Array<float>, 2> opHandsKeypoints;
+
+const opFacesKeypoints NOFACES;
+const opHandsKeypoints NOHANDS{};
+
+YAML::Node makeYamlFrame(const opPosesKeypoints poses, const opFacesKeypoints faces, const opHandsKeypoints hands) {
+
+    YAML::Node node;
+
+    const int NB_POSE_KEYPOINTS = 18; // COCO_18
+    const int NB_FACE_KEYPOINTS = 70; // COCO_18
+    const int NB_HAND_KEYPOINTS = 21; // COCO_18
+
+    // pose
+    for (int i = 0; i < poses.getSize(0); i++) {
+        if(poses.getSize(1) != NB_POSE_KEYPOINTS) throw range_error("Unexpected number of pose keypoints!");
+
+        for (int idx = 0; idx < NB_POSE_KEYPOINTS; idx++) {
+            node["poses"][i+1][idx+1] = std::vector<float>({poses.at({i,idx,0}), poses.at({i,idx,1}), poses.at({i,idx,2})});
+        }
+    }
+
+    // face
+    if (!faces.empty())
+    {
+        for (int i = 0; i < faces.getSize(0); i++) {
+            if(faces.getSize(1) != NB_FACE_KEYPOINTS) throw range_error("Unexpected number of face keypoints! Expected " + to_string(NB_FACE_KEYPOINTS) + ", got " + to_string(faces.getSize(1)));
+
+            for (int idx = 0; idx < NB_FACE_KEYPOINTS; idx++) {
+                node["faces"][i+1][idx+1] = std::vector<float>({faces.at({i,idx,0}), faces.at({i,idx,1}), faces.at({i,idx,2})});
+            }
+        }
+    }
+
+    if (!hands.empty())
+    {
+        // left hand
+        for (int i = 0; i < hands[0].getSize(0); i++) {
+            if(hands[0].getSize(1) != NB_HAND_KEYPOINTS) throw range_error("Unexpected number of left hand keypoints! Expected " + to_string(NB_HAND_KEYPOINTS) + ", got " + to_string(hands[0].getSize(1)));
+
+
+            for (int idx = 0; idx < NB_HAND_KEYPOINTS; idx++) {
+                node["hands"]["left"][i+1][idx+1] = std::vector<float>({hands[0].at({i,idx,0}), hands[0].at({i,idx,1}), hands[0].at({i,idx,2})});
+            }
+        }
+
+        // right hand
+        for (int i = 0; i < hands[1].getSize(0); i++) {
+            if(hands[1].getSize(1) != NB_HAND_KEYPOINTS) throw("Unexpected number of right hand keypoints! Expected " + to_string(NB_HAND_KEYPOINTS) + ", got " + to_string(hands[1].getSize(1)));
+
+            for (int idx = 0; idx < NB_HAND_KEYPOINTS; idx++) {
+                node["hands"]["right"][i+1][idx+1] = std::vector<float>({hands[1].at({i,idx,0}), hands[1].at({i,idx,1}), hands[1].at({i,idx,2})});
+            }
+        }
+    }
+
+
+    return node;
+    
+}
 
 int main(int argc, char **argv) {
 
@@ -64,8 +183,10 @@ int main(int argc, char **argv) {
         ("help,h", "produces help message")
         ("version,v", "shows version and exits")
         ("topic", po::value<string>(), "topic to process (must be of type CompressedImage)")
-        ("model", po::value<string>(), "dlib's trained face model")
+        ("models", po::value<string>()->default_value("models/"), "path to OpenPose models")
         ("path", po::value<string>(), "record path (must contain experiment.yaml and freeplay.bag)")
+        ("face", po::value<bool>()->default_value(true), "detect faces as well")
+        ("hand", po::value<bool>()->default_value(true), "detect hands as well")
         ;
 
     po::variables_map vm;
@@ -87,12 +208,6 @@ int main(int argc, char **argv) {
 
     if (vm.count("topic") == 0) {
         cout << "You must specify a topic to process" << endl;
-        return 1;
-    }
-
-    if (vm.count("model") == 0) {
-        cout << "You must specify the path to a trained dlib's face model\n"
-            << "with the option --model." << endl;
         return 1;
     }
 
@@ -121,6 +236,8 @@ int main(int argc, char **argv) {
     const auto heatMapScale = op::ScaleMode::UnsignedChar;
     
     // Configure OpenPose
+    google::InitGoogleLogging("openPoseTutorialWrapper1");
+    op::ConfigureLog::setPriorityThreshold(op::Priority::NoOutput);
     op::Wrapper<std::vector<op::Datum>> opWrapper{op::ThreadManagerMode::Asynchronous};
 
     // Pose configuration (use WrapperStructPose{} for default and recommended configuration)
@@ -137,13 +254,13 @@ int main(int argc, char **argv) {
                                                   0.6, // alpha blending pose
                                                   0.7, // alpha blending heatmap
                                                   0, // part to show, 0 shows all
-                                                  "models/", // models folder
+                                                  vm["models"].as<string>(), // models folder
                                                   heatMapTypes, heatMapScale, 
                                                   0.05, // render threshold -- only render part with threshold above it
                                                 };
 
     // Face configuration (use op::WrapperStructFace{} to disable it)
-    const op::WrapperStructFace wrapperStructFace{true, // enables face detection
+    const op::WrapperStructFace wrapperStructFace{vm["face"].as<bool>(), // enables face detection
                                                   faceNetInputSize, 
                                                   op::RenderMode::Cpu, // render a bit faster on CPU
                                                   0.6, // alpha blending pose
@@ -152,7 +269,7 @@ int main(int argc, char **argv) {
                                                 };
 
     // Hand configuration (use op::WrapperStructHand{} to disable it)
-    const op::WrapperStructHand wrapperStructHand{true, // enables hand detection
+    const op::WrapperStructHand wrapperStructHand{vm["hand"].as<bool>(), // enables hand detection
                                                   handNetInputSize,
                                                   1, // Analogous to `scale_number` but applied to the hand keypoint detector. Our best results were found with `hand_scale_number` = 6 and `hand_scale_range` = 0.4
                                                   0.4f, // hand_scale_range
@@ -187,39 +304,63 @@ int main(int argc, char **argv) {
                         op::WrapperStructInput{}, 
                         wrapperStructOutput);
 
+    // openpose -- start
+    cout << "Starting the pose estimator thread" << endl;
+    opWrapper.start();
+
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
-    
+   
     //auto estimator = HeadPoseEstimation(vm["model"].as<string>());
 
-    cout << "Openning " << vm["path"].as<string>() << "/freeplay.bag..." << endl;
+    cout << "Opening " << vm["path"].as<string>() << "/freeplay.bag..." << endl;
     rosbag::Bag bag(vm["path"].as<string>() + "/freeplay.bag", rosbag::bagmode::Read);
 
-    string image_topic(vm["topic"].as<string>());
+    string topic(vm["topic"].as<string>());
 
     std::vector<std::string> topics;
-    topics.push_back(image_topic);
+    topics.push_back(topic);
 
     rosbag::View view(bag, rosbag::TopicQuery(topics));
     //rosbag::View view(bag);
 
     cout << view.size() << " messages to process" << endl << endl;
 
-    int nb_images = 0;
     int nb_images_with_face = 0;
-    vector<int> multiples_faces_frames;
-    int nb_msgs = 0;
     int last_percent=0;
 
-    // openpose -- start
-    cout << "Starting the pose estimator thread" << endl;
-    opWrapper.start();
 
+    YAML::Node posesYaml = YAML::Load(topic + ": []");
+    try {
+        cout << "Opening existing " << POSES_FILE << "...";
+        posesYaml = YAML::LoadFile(vm["path"].as<string>() + "/" + POSES_FILE);
+        cout << "done." << endl;
+    }
+    catch (YAML::BadFile bf) {
+        cout << "Creating new " << POSES_FILE << endl;
+    }
+    
+
+    YAML::Node yamlNode = posesYaml[vm["topic"].as<string>()]["frames"];
+
+    auto nbAlreadyProcessed = yamlNode.size();
+
+    if(nbAlreadyProcessed > 0) {
+        if(nbAlreadyProcessed == view.size()) {
+            cout << "All frames alreay processed. Quitting now." << endl;
+            return 0;
+        }
+        cout << "Already " << nbAlreadyProcessed << " frames processed for this topic. Skipping them." << endl;
+    }
+
+    size_t idx = 0;
 
     for(rosbag::MessageInstance const m : view)
     {
-        nb_msgs++;
+        idx++;
+
+        if(idx <= nbAlreadyProcessed) continue;
 
         auto compressed_rgb = m.instantiate<sensor_msgs::CompressedImage>();
         if (compressed_rgb != NULL) {
@@ -238,58 +379,45 @@ int main(int argc, char **argv) {
             // Pop frame
             std::shared_ptr<std::vector<op::Datum>> datumProcessed;
             if (successfullyEmplaced && opWrapper.waitAndPop(datumProcessed)) {
-                imshow("pose", datumProcessed->at(0).cvOutputData);
-                waitKey(10);
-                cout << "Frame successfully processed!" << endl;
+                auto yamlFrame = makeYamlFrame(
+                                          datumProcessed->at(0).poseKeypoints,
+                                          vm["face"].as<bool>() ? datumProcessed->at(0).faceKeypoints : NOFACES,
+                                          vm["hand"].as<bool>() ? datumProcessed->at(0).handKeypoints : NOHANDS);
+                yamlNode.push_back(yamlFrame);
+
+                if (datumProcessed->at(0).faceRectangles.size() > 0) {
+                    nb_images_with_face++;
+                }
+
+                //imshow("pose", datumProcessed->at(0).cvOutputData);
+                //waitKey();
             }
             else
                 cerr << "Failed to emplace frame" << endl;
 
-
-            //if (res.size() > 0) {
-            //    if(res.size() > 1) {
-            //        cout << "Frame " << nb_images << " of topic " << m.getTopic() << ": Found more than one face!" << endl;
-            //        multiples_faces_frames.push_back(nb_msgs);
-            //    }
-            //    nb_images_with_face++;
-            //}
-
-            //nb_images++;
-            
-            
-            
-            
-            //if (show_frame) {
-            //    imshow("headpose", estimator._debug);
-            //    waitKey(10);
-            //}
-
         }
 
-        int percent = nb_msgs * 100 / view.size();
+        int percent = idx * 100 / view.size();
         if (percent != last_percent) {
-            cout << "\x1b[FDone " << percent << "% (" << nb_images << " images)" << endl;
+            cout << "\x1b[FDone " << percent << "% (" << idx << " images)" << endl;
             last_percent = percent;
         }
 
-        if(interrupted) break;
+        if(interrupted) {
+            cout << "Interrupted." << endl;
+            break;
+        }
     }
 
-    cout << "Found " << nb_images_with_face << " images with faces out of " << nb_images << " (" << (nb_images_with_face * 100.f)/nb_images << "%)" << endl;
 
+    cout << "Found " << nb_images_with_face << " images with faces out of " << idx << " (" << (nb_images_with_face * 100.f)/idx << "%)" << endl;
 
-    YAML::Node facesyaml = YAML::Load("faces: []");
-    try {
-        facesyaml = YAML::LoadFile(vm["path"].as<string>() + "/faces.yaml");
-        cout << "Updating existing faces.yaml" << endl;
+    if(vm["face"].as<bool>()) {
+        posesYaml[topic]["nb_frames_with_face"] = nb_images_with_face;
     }
-    catch (YAML::BadFile bf) {
-        cout << "Creating new faces.yaml" << endl;
-    }
-    facesyaml["faces"][vm["topic"].as<string>()]["nb_frames"] = nb_images;
-    facesyaml["faces"][vm["topic"].as<string>()]["nb_frames_with_face"] = nb_images_with_face;
-    facesyaml["faces"][vm["topic"].as<string>()]["multiple_faces"] = multiples_faces_frames;
-    std::ofstream fout(vm["path"].as<string>() + "/faces.yaml");
-    fout << facesyaml;
+
+    std::ofstream fout(vm["path"].as<string>() + "/" + POSES_FILE);
+    fout << posesYaml;
+
     bag.close();
 }

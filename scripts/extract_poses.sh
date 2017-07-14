6d8eebf899426273a5b10582fa0565a76ded11bd
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2017-07-14 14:41:36 +0100

    Adapt detect_faces.sh to OpenPose + change name

---
+++ scripts/extract_poses.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+EXEC=$HOME/src/freeplay-sandbox-analysis/build/devel/lib/freeplay_sandbox_analysis/faces_analysis
+DATASET_ROOT=/media/$USER/DoRoThy1/freeplay_sandox/data
+
+OPENPOSE_MODELS=/opt/openpose/models
+
+orig_path=$(pwd)
+
+cd $DATASET_ROOT
+paths=(2017**/experiment.yaml)
+
+
+for ((i=0;i<${#paths[@]};i++))
+do
+    f=${paths[$i]}
+    
+    dir=$(dirname $f)
+    
+    if [ ! -f $dir/poses.json ]; then
+    
+        echo "Detecting faces for experiment $f (experiment $((i+1))/${#paths[@]}..."
+        $EXEC --model=$OPENPOSE_MODELS $dir
+    
+    else
+        echo "poses.json already exist. Skipping $dir."
+    fi
+done
+
+cd $orig_path
+echo "Done"

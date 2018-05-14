c895d34d47ccccdc8b7691f96357bfc4f2f58569
Séverin Lemaignan <severin.lemaignan@brl.ac.uk>
2018-05-14 10:28:20 +0100

    Added anthropometric for 8yo children

--- src/head_pose_estimator.hpp
+++ src/head_pose_estimator.hpp
@@ -10,10 +10,15 @@
 
 #include "gaze_features.hpp" // for the 'feature' typedef
 
-// Anthropometric for male adult
-// Relative position of various facial feature relative to sellion
-// Values taken from https://en.wikipedia.org/wiki/Human_head
+// ****** Anthorpometrics of the head ******
+// Relative position of various facial feature relative to sellion (in mm)
 // X points forward
+
+#define ADULT
+
+// Anthropometric for male adult
+// Values taken from https://en.wikipedia.org/wiki/Human_head
+#ifdef ADULT
 const static cv::Point3f P3D_SELLION(0., 0.,0.);
 const static cv::Point3f P3D_RIGHT_EYE(-20., -65.5,-5.);
 const static cv::Point3f P3D_LEFT_EYE(-20., 65.5,-5.);
@@ -22,8 +27,24 @@ const static cv::Point3f P3D_LEFT_EAR(-100., 77.5,-6.);
 const static cv::Point3f P3D_NOSE(21.0, 0., -48.0);
 const static cv::Point3f P3D_STOMMION(10.0, 0., -75.0);
 const static cv::Point3f P3D_MENTON(0., 0.,-133.0);
+#endif
+// Anthropometrics for children (8 year old), taken from https://math.nist.gov/~SRessler/anthrokids/
+// (US survey from 1977)
+// (some missing values are interpolated/estimated, in particular for eyes)
+// Relative position of various facial feature relative to sellion (in mm)
+// X points forward
+#ifdef CHILD
+const static cv::Point3f P3D_SELLION(0., 0.,0.);
+const static cv::Point3f P3D_RIGHT_EYE(-20., -35.,-1.);
+const static cv::Point3f P3D_LEFT_EYE(-20., 35.,-1.);
+const static cv::Point3f P3D_RIGHT_EAR(-89., -58,-6.);
+const static cv::Point3f P3D_LEFT_EAR(-89., 58,-6.);
+const static cv::Point3f P3D_NOSE(15.0, 0., -31.0);
+const static cv::Point3f P3D_STOMMION(0., 0., -62.0);
+const static cv::Point3f P3D_MENTON(-5., 0.,-93.0);
+#endif
 
-
+//*************************************
 
 typedef cv::Matx44d head_pose;
 
